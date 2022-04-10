14d4aca20f78b21da92e3616382e238ea855392f
Yuliya_Anel <julija.anjol@gmail.com>
2022-04-10 18:50:54 +0300

    git -m "add files"

--- src/Homework/HW_2/hw_2.java
+++ src/Homework/HW_2/hw_2.java
@@ -11,6 +11,7 @@ public class hw_2 {
                 + MIN_VALUE);
 
         int i = MIN_VALUE;
+        int t = Integer.MAX_VALUE;
         System.out.println(
                         ".             .  . . . . . .   .              .                . . . . . .\n" +
                         ".             .  .             .              .                .         .\n" +
@@ -24,5 +25,29 @@ public class hw_2 {
         System.out.println(Integer.compare(a, b));
         Float f = 234.9999F;
         System.out.println(Math.round(f));
+
+        int c = 23;
+        int expectedResult = 69;
+
+         if ( c % 2 != 0) {
+             if (c % 3 == 0) {
+                 c = c / 3;
+             }   else {
+                 c = c * 3;
+             }
+         } else {
+             c = c * (c / 2);
+         }
+        System.out.println(c);
+
+    if (expectedResult == c) {
+        System.out.println("\u001B[32m" + "passed");
+    } else {
+        System.out.println("\u001B[31m" + "failed");
+        }
+    double result = 10.0 / 3;
+        System.out.println(result);
+    double r = ((double) 10) / 3;
+        System.out.println(r);
     }
 }
